dd4d1edd26ad806f7f628687e6dfc7842ad80e17
[caizhenghe] <554112503@qq.com>
2018-10-24 01:07:56 +0800

    asc: add save image to album func

--- ASCPicture/app/src/main/java/com/czh/ascpicture/BitmapUtils.java
+++ ASCPicture/app/src/main/java/com/czh/ascpicture/BitmapUtils.java
@@ -1,28 +1,37 @@
 package com.czh.ascpicture;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.WindowManager;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Administrator on 2018/10/22.
  */
 
 public class BitmapUtils {
-    public static DisplayMetrics getSreenMethics(Context context) {
+    private static final int DEFAULT_SCALE = 7;
+
+    public static DisplayMetrics getScreenMetrics(Context context) {
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(dm);
@@ -41,7 +50,7 @@ public class BitmapUtils {
             }
         }
 
-        return 8;
+        return inSampleSize;
     }
 
     public static Bitmap decodeBitmapFromRes(Resources resources, int resId, int reqWidth, int reqHeight) {
@@ -50,36 +59,32 @@ public class BitmapUtils {
         BitmapFactory.decodeResource(resources, resId, options);
         options.inSampleSize = getInSampleSize(options, reqWidth, reqHeight);
         options.inJustDecodeBounds = false;
-        Bitmap bm = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, resId, options), reqWidth / 7, reqHeight / 7, true);
-        return bm;
+        return BitmapFactory.decodeResource(resources, resId, options);
     }
 
-    public static Bitmap createAsciiPic(Resources resources,int resId, Context context) {
+    public static Bitmap decodeBitmapFromPath(Context context, String path) {
+        Bitmap originBitmap = BitmapFactory.decodeFile(path);
+        return scale(context, originBitmap);
+    }
+
+    public static Bitmap decodeBitmapFromResource(Context context, Resources res, int resId) {
+        Bitmap originBitmap = BitmapFactory.decodeResource(res, resId);
+        return scale(context, originBitmap);
+    }
+
+    public static Bitmap decodeBitmapFromStream(Context context, InputStream stream) {
+        Bitmap originBitmap = BitmapFactory.decodeStream(stream);
+        return scale(context, originBitmap);
+    }
+
+    public static Bitmap createAsciiPic(Context context, Bitmap bitmap) {
         final String base = "#8XOHLTI)i=+;:,.";// 字符串由复杂到简单
 //        final String base = "#,.0123456789:;@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";// 字符串由复杂到简单
         StringBuilder text = new StringBuilder();
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        Bitmap image = BitmapFactory.decodeResource(resources, resId);  //读取图片
-        int width0 = image.getWidth();
-        int height0 = image.getHeight();
-        int width1, height1;
-        int scale = 7;
-        if (width0 <= width / scale) {
-            width1 = width0;
-            height1 = height0;
-        } else {
-            width1 = width / scale;
-            height1 = width1 * height0 / width0;
-        }
-        image = scale(image, width1, height1);  //读取图片
-        //输出到指定文件中
-        for (int y = 0; y < image.getHeight(); y += 2) {
-            for (int x = 0; x < image.getWidth(); x++) {
-                final int pixel = image.getPixel(x, y);
+        // 根据Bitmap的灰度将Bitmap转换成字符串
+        for (int y = 0; y < bitmap.getHeight(); y += 2) {
+            for (int x = 0; x < bitmap.getWidth(); x++) {
+                final int pixel = bitmap.getPixel(x, y);
                 final int r = (pixel & 0xff0000) >> 16, g = (pixel & 0xff00) >> 8, b = pixel & 0xff;
                 final float gray = 0.299f * r + 0.578f * g + 0.114f * b;
                 final int index = Math.round(gray * (base.length() + 1) / 255);
@@ -88,63 +93,83 @@ public class BitmapUtils {
             }
             text.append("\n");
         }
-        return textAsBitmap(text, context);
-//        return creatCodeBitmap(text,context,colors);
-//        return image;
+        return textAsBitmap(context, text);
     }
 
-    public static Bitmap scale(Bitmap bitmap, int newWidth, int newHeight) {
-        Bitmap ret = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
-        return ret;
-    }
-
-//    public static Bitmap createAsciiPic(Context context, Bitmap bitmap) {
-//        final String base = "#8XOHLTI)i=+;:,.";// 字符串由复杂到简单
-////        final String base = "#,.0123456789:;@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";// 字符串由复杂到简单
-//        StringBuilder text = new StringBuilder();
-//        //输出到指定文件中
-//        for (int y = 0; y < bitmap.getHeight(); y += 2) {
-//            for (int x = 0; x < bitmap.getWidth(); x++) {
-//                final int pixel = bitmap.getPixel(x, y);
-//                final int r = (pixel & 0xff0000) >> 16, g = (pixel & 0xff00) >> 8, b = pixel & 0xff;
-//                final float gray = 0.299f * r + 0.578f * g + 0.114f * b;
-//                final int index = Math.round(gray * (base.length() + 1) / 255);
-//                String s = index >= base.length() ? " " : String.valueOf(base.charAt(index));
-//                text.append(s);
-//            }
-//            text.append("\n");
-//        }
-//        return textAsBitmap(text, context);
-//    }
-
-    public static Bitmap textAsBitmap(StringBuilder text, Context context) {
+    public static Bitmap textAsBitmap(Context context, StringBuilder text) {
         TextPaint textPaint = new TextPaint();
         textPaint.setColor(Color.GRAY);
         textPaint.setAntiAlias(true);
         textPaint.setTypeface(Typeface.MONOSPACE);
+        // FIXME: 2018/10/23 textSize如何得出？一个字符大约在7px左右
+        // TODO: 2018/10/24 字符宽度、originBitmap宽度和屏幕宽度之间的关系
         textPaint.setTextSize(12);
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;         //
+        int screenWidth = getScreenMetrics(context).widthPixels;
 
-        StaticLayout layout = new StaticLayout(text, textPaint, width,
+        StaticLayout layout = new StaticLayout(text, textPaint, screenWidth,
                 Layout.Alignment.ALIGN_CENTER, 1f, 0.0f, true);
 
         Bitmap bitmap = Bitmap.createBitmap(layout.getWidth() + 20,
                 layout.getHeight() + 20, Bitmap.Config.ARGB_8888);
 
         Canvas canvas = new Canvas(bitmap);
-
         canvas.translate(10, 10);
-
         canvas.drawColor(Color.WHITE);
-
 //        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//绘制透明色
-
+        /**
+         * 若一行文字的内容超出行宽限制，则自动换行，且Layout设置了居中对齐
+         */
         layout.draw(canvas);
 
         return bitmap;
 
+    }
+
+    public static void saveBitmapToSysAlbum(Context context, Bitmap bitmap) {
+        if(bitmap == null)
+            return;
+        String fileName = System.currentTimeMillis() / 1000 + ".jpeg";
+        String filePath;
+        if (Build.BRAND.equals("Xiaomi")) {//小米
+            filePath = Environment.getExternalStorageDirectory() + "/DCIM/Camera/" + fileName;
+        } else {// 魅族、OPPO
+            filePath = Environment.getExternalStorageDirectory() + "/DCIM/" + fileName;
+        }
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            // 将Bitmap转换输出到JPEG格式的文件中
+            if (bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)) {
+                fos.flush();
+                fos.close();
+                // optional:将图片插入图库
+                //MediaStore.Images.Media.insertImage(context.getContentResolver(), filePath, fileName, null);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // optional:通知图库刷新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + filePath)));
+    }
+
+    private static Bitmap scale(Context context, Bitmap originBitmap) {
+        // 比较原图宽度和屏幕宽度/scale,取小值，要求保持原图宽高比例不变
+        // FIXME: 2018/10/23 scale如何得出
+        int reqWidth, reqHeight;
+        DisplayMetrics metrics = getScreenMetrics(context);
+        if (originBitmap.getWidth() <= metrics.widthPixels / DEFAULT_SCALE) {
+            reqWidth = originBitmap.getWidth();
+            reqHeight = originBitmap.getHeight();
+        } else {
+            reqWidth = (int) metrics.widthPixels / DEFAULT_SCALE;
+            reqHeight = originBitmap.getHeight() * reqWidth / originBitmap.getWidth();
+        }
+
+        return Bitmap.createScaledBitmap(originBitmap, reqWidth, reqHeight, true);
     }
 }
--- ASCPicture/app/src/main/java/com/czh/ascpicture/MainActivity.java
+++ ASCPicture/app/src/main/java/com/czh/ascpicture/MainActivity.java
@@ -1,18 +1,26 @@
 package com.czh.ascpicture;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.BoringLayout;
-import android.text.style.MetricAffectingSpan;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    Bitmap mBitmap;
-    Bitmap mAscBitmap;
-    ImageView mImageView;
+    private static final String ROOT_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private static final int ALBUM_REQ_ID = 100;
+
+    private boolean mIsAdd;
+    private Bitmap mOutBitmap;
+
+    private ImageView mImageView, mAddIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +28,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mImageView = (ImageView) findViewById(R.id.marie_iv);
+        mImageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                BitmapUtils.saveBitmapToSysAlbum(MainActivity.this, mOutBitmap);
+                Toast.makeText(MainActivity.this, "图片已保存", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mAddIv = (ImageView) findViewById(R.id.add_iv);
+    }
+
+    public void doClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_iv:
+                openSystemAlbum();
+                break;
+            case R.id.marie_iv:
+                mIsAdd = false;
+                updateImageView(null);
+                mOutBitmap = null;
+                Toast.makeText(this, "图片已删除", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ALBUM_REQ_ID) {
+            if (data != null) {
+                try {
+                    InputStream is = getContentResolver().openInputStream(data.getData());
+                    Bitmap originBitmap = BitmapUtils.decodeBitmapFromStream(this, is);
+                    showBitmap(originBitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private void openSystemAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, ALBUM_REQ_ID);
+    }
+
+    private void showBitmap(final Bitmap originBitmap) {
+        String testPath = ROOT_PATH + "/DCIM/m8.jpg";
         new Thread(new Runnable() {
             @Override
             public void run() {
-//                DisplayMetrics metrics = BitmapUtils.getSreenMethics(MainActivity.this);
-//                mBitmap = BitmapUtils.decodeBitmapFromRes(getResources(), R.drawable.icon, metrics.widthPixels, metrics.heightPixels);
-                mAscBitmap = BitmapUtils.createAsciiPic(getResources(), R.drawable.m9, MainActivity.this);
+                mOutBitmap = BitmapUtils.createAsciiPic(MainActivity.this, originBitmap);
                 mImageView.post(new Runnable() {
                     @Override
                     public void run() {
-                        mImageView.setImageBitmap(mAscBitmap);
+                        mIsAdd = true;
+                        updateImageView(mOutBitmap);
+
                     }
                 });
             }
         }).start();
     }
 
-    public void doClick(View v) {
-        mImageView.setImageBitmap(mBitmap);
+    private void updateImageView(Bitmap bitmap) {
+        if (mIsAdd) {
+            mAddIv.setVisibility(View.GONE);
+            mImageView.setVisibility(View.VISIBLE);
+            mImageView.setImageBitmap(bitmap);
+        } else {
+            mAddIv.setVisibility(View.VISIBLE);
+            mImageView.setImageBitmap(null);
+            mImageView.setVisibility(View.GONE);
+        }
     }
+
+
 }
