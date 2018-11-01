4567c3961e4b83b2a494edbe36da800d46fa199b
caizhenghe <caizhenghe@tp-link.com.cn>
2018-11-01 09:15:01 +0800

    asc: add ffmpeg to asc project

--- ASCPicture/app/src/main/jni/Android.mk
+++ ASCPicture/app/src/main/jni/Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+LIB_PATH := $(LOCAL_PATH)/../../../libs/armeabi-v7a
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ffmpeg
-LOCAL_SRC_FILES := libffmpeg.so
+LOCAL_SRC_FILES := $(LIB_PATH)/libffmpeg.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ffmpeginvoke
-LOCAL_SRC_FILES := codepig_ffmpegcldemo_FFmpegKit.c ffmpeg.c ffmpeg_opt.c cmdutils.c ffmpeg_filter.c
-LOCAL_C_INCLUDES := com_czh_ascpicture_FFmpegKit.c
+LOCAL_SRC_FILES := com_czh_ascpicture_FFmpegKit.c ffmpeg.c ffmpeg_opt.c cmdutils.c ffmpeg_filter.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_LDLIBS := -llog -lz -ldl
 LOCAL_SHARED_LIBRARIES := ffmpeg
 include $(BUILD_SHARED_LIBRARY)
