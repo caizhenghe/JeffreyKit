81ce27802a7902f8c7bd0b367e55c44e26b3a934
caizhenghe <caizhenghe@tp-link.com.cn>
2018-11-13 09:51:31 +0800

    Tax: udpate fund/security talculate method

--- Tax/app/src/main/java/com/czh/tax/MainActivity.java
+++ Tax/app/src/main/java/com/czh/tax/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText mSalaryInputEt, mTakeOffInputEt;
+    private EditText mSalaryInputEt, mOldSalaryInputEt, mTakeOffInputEt;
     private TextView mRealSalaryTv, mTaxTv, mFundTv, mSecurityTv;
     private ImageView mResidenceIv;
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSalaryInputEt = (EditText) findViewById(R.id.input_salary_et);
+        mOldSalaryInputEt = (EditText) findViewById(R.id.input_old_salary_et);
         mTakeOffInputEt = (EditText) findViewById(R.id.input_takeoff_et);
         mRealSalaryTv = (TextView) findViewById(R.id.real_salary_tv);
         mTaxTv = (TextView) findViewById(R.id.tax_tv);
@@ -49,20 +51,22 @@ public class MainActivity extends AppCompatActivity {
     public void doClick(View v) {
         switch (v.getId()) {
             case R.id.calculate_btn:
+                String oldSalaryInput = "";
+                boolean isOldSalaryVisible = mOldSalaryInputEt.getVisibility() == View.VISIBLE;
+                oldSalaryInput = mOldSalaryInputEt.getText().toString();
                 String salaryInput = mSalaryInputEt.getText().toString();
                 String takeOffInput = mTakeOffInputEt.getText().toString();
-                if (!TaxUtils.isNum(salaryInput)) {
-                    Toast.makeText(this, "请输入数字！", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(!TaxUtils.isNum(takeOffInput)) {
+                if (!TaxUtils.isNum(salaryInput)
+                        || !TaxUtils.isNum(takeOffInput)
+                        || isOldSalaryVisible && !TaxUtils.isNum(oldSalaryInput)) {
                     Toast.makeText(this, "请输入数字！", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 int salary = TextUtils.isEmpty(salaryInput) ? 0 : Integer.valueOf(salaryInput);
+                int oldSalary = TextUtils.isEmpty(salaryInput) ? 0 : Integer.valueOf(oldSalaryInput);
                 int extraTakeOff = TextUtils.isEmpty(takeOffInput) ? 0 : Integer.valueOf(takeOffInput);
-                float fund = TaxUtils.getFunds(salary, TaxUtils.DEFAULT_FUNDS_RATE);
-                float security = TaxUtils.getSecurity(salary, mMedicalLevel);
+                float fund = TaxUtils.getFunds(isOldSalaryVisible ? oldSalary : salary, TaxUtils.DEFAULT_FUNDS_RATE);
+                float security = TaxUtils.getSecurity(isOldSalaryVisible ? oldSalary : salary, mMedicalLevel);
                 float tax = TaxUtils.getTax(this, salary, security, fund, extraTakeOff);
                 float result = salary - tax - fund - security - extraTakeOff;
                 mFundTv.setText("应缴公积金：" + String.format("%.2f", fund));
@@ -84,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
+        mOldSalaryInputEt.setVisibility(View.GONE);
         switch (item.getItemId()) {
             case R.id.is_sz_residence:
                 mMedicalLevel = TaxUtils.FIRST_MEDICAL_SECURITY_LEVEL;
@@ -92,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.is_not_sz_residence:
                 mMedicalLevel = TaxUtils.SECOND_MEDICAL_SECURITY_LEVEL;
                 Toast.makeText(this, "已切换至非深户", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.change_salary_recently_for_sz_residence:
+                mOldSalaryInputEt.setVisibility(View.VISIBLE);
                 return true;
         }
         return super.onContextItemSelected(item);
