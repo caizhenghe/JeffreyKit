97c5d1931d0dc23526cc911c32888b62d505f83d
caizhenghe <caizhenghe@tp-link.com.cn>
2019-01-31 09:25:24 +0800

    AIDL: add death proxy

--- AIDLClient/app/src/main/java/com/tplink/sdk/aidlclient/MainActivity.java
+++ AIDLClient/app/src/main/java/com/tplink/sdk/aidlclient/MainActivity.java
@@ -3,6 +3,7 @@ package com.tplink.sdk.aidlclient;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -19,10 +20,29 @@ public class MainActivity extends AppCompatActivity {
     private IBookManager mIBookManager;
     private int mBookId = 0;
 
+    IBinder.DeathRecipient Recipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            if (mIBookManager == null)
+                return;
+            mIBookManager.asBinder().unlinkToDeath(Recipient, 0);
+            mIBookManager = null;
+            // 重新绑定远程service
+            Intent intent = new Intent();
+            ComponentName name = new ComponentName("com.tplink.sdk.aidlserver", "com.tplink.sdk.aidlserver.AIDLService");
+            intent.setComponent(name);
+            bindService(intent, mConn, BIND_AUTO_CREATE);
+        }
+    };
     ServiceConnection mConn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             mIBookManager = IBookManager.Stub.asInterface(service);
+            try {
+                service.linkToDeath(Recipient, 0);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -42,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void doClick(View v){
-        switch (v.getId()){
+    public void doClick(View v) {
+        switch (v.getId()) {
             case R.id.btn_add_book:
-                if(mIBookManager!= null) {
+                if (mIBookManager != null) {
                     mBookId++;
                     Book book = new Book();
                     book.bookId = mBookId;
@@ -58,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.btn_get_book_list:
-                if(mIBookManager!= null) {
+                if (mIBookManager != null) {
                     try {
                         List<Book> bookList = mIBookManager.getBookList();
                         TextView tv = findViewById(R.id.tv_book_list);
