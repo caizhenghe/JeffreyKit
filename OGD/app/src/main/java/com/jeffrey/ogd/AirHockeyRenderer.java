8be0ee0a8930de8f18ff3ad3109cf7d0c1c05ef1
caizhenghe <caizhenghe@bilibili.com>
2019-07-31 18:18:22 +0800

    opengl: adapter ratio of width/height in landscape/vertical screen

--- OGD/app/src/main/java/com/jeffrey/ogd/AirHockeyRenderer.java
+++ OGD/app/src/main/java/com/jeffrey/ogd/AirHockeyRenderer.java
@@ -3,6 +3,7 @@ package com.jeffrey.ogd;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -27,45 +28,34 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final String A_COLOR = "a_Color";
     private static final String A_POSITION = "a_Position";
+    private static final String U_MATRIX = "u_Matrix";
     private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
+    private static final float[] mProjectMatrix = new float[16];
 
     //    private int mUColorLocation;
     private int mAColorLocation;
     private int mAPositionLocation;
+    private int mUMatrixLocation;
 
     /**
      * 三角形扇，中间点进行重用
      * 将顶点位置和颜色记录在一个数组当中
      */
-//    private float[] mTableVerticesWithTriangles = {
-//            // triangle fan
-//            0, 0, 1f, 1f, 1f,
-//            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-//            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-//            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-//            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-//            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-//            // line1
-//            -0.5f, 0f, 1f, 0f, 0f,
-//            0.5f, 0f, 1f, 0f, 0f,
-//            // mallets
-//            0f, -0.25f, 0f, 0f, 1f,
-//            0f, 0.25f, 1f, 0f, 0f,
-//    };
     private float[] mTableVerticesWithTriangles = {
             // triangle fan
             0, 0, 1f, 1f, 1f,
-            -1f, -1f, 0.7f, 0.7f, 0.7f,
-            1f, -1f, 0.7f, 0.7f, 0.7f,
-            1f, 1f, 0.7f, 0.7f, 0.7f,
-            -1f, 1f, 0.7f, 0.7f, 0.7f,
-            -1f, -1f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
             // line1
-            -1f, 0f, 1f, 0f, 0f,
-            1f, 0f, 1f, 0f, 0f,
+            -0.5f, 0f, 1f, 0f, 0f,
+            0.5f, 0f, 1f, 0f, 0f,
             // mallets
-            0f, -0.5f, 0f, 0f, 1f,
-            0f, 0.5f, 1f, 0f, 0f,
+            0f, -0.4f, 0f, 0f, 1f,
+            0f, 0.4f, 1f, 0f, 0f,
     };
 
     /**
@@ -101,6 +91,20 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     public AirHockeyRenderer(Context context) {
         mContext = context;
+
+        // 判断横竖屏
+//        Configuration configuration = context.getResources().getConfiguration();
+//        // 横竖屏适配方案一：横屏下动态修改矩阵坐标，保证图像宽高比不变
+//        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            // 获取屏幕宽高
+//            DisplayMetrics dm = context.getResources().getDisplayMetrics();
+//            float ratio = dm.heightPixels / (float) dm.widthPixels;
+//            // 适配图像的x坐标
+//            for (int i = 0; i < mTableVerticesWithTriangles.length; i += 5) {
+//                mTableVerticesWithTriangles[i] = mTableVerticesWithTriangles[i] * ratio;
+//            }
+//        }
+
         // 创建顶点坐标的本地内存
         mVertexData = ByteBuffer
                 .allocateDirect(mTableVerticesWithTriangles.length * BYTES_PER_FLOAT)
@@ -124,6 +128,8 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         mAColorLocation = glGetAttribLocation(mProgram, A_COLOR);
         // 从通道中取出Position属性的位置
         mAPositionLocation = glGetAttribLocation(mProgram, A_POSITION);
+        // 从通道中取出正交投影矩阵的位置
+        mUMatrixLocation = glGetUniformLocation(mProgram, U_MATRIX);
 
         // 将本地内存中的顶点坐标传给OpenGL
         mVertexData.position(0);
@@ -147,12 +153,25 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         // 设置视口尺寸（顶点坐标矩阵会映射到该视窗中），View的左下角是原点(0, 0)
         glViewport(0, 0, width, height);
+
+        // 方案二：通过宽高比定义虚拟坐标空间（拓宽x，y轴空间），再通过正交投影矩阵将虚拟坐标转换到归一化设备坐标
+        // 创建正交矩阵
+        final float ratio = width > height ? width / (float) height : height / (float) width;
+        if (width > height) {
+            // 横屏状态下拓宽x轴范围
+            Matrix.orthoM(mProjectMatrix, 0, -ratio, ratio, -1f, 1f, -1f, 1f);
+        } else {
+            // 竖屏状态下拓宽y轴范围
+            Matrix.orthoM(mProjectMatrix, 0, -1f, 1f, -ratio, ratio, -1f, 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         // 使用之前定义的擦除颜色来擦除屏幕上的所有颜色
         glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        // 将正交矩阵传递给着色器
+        glUniformMatrix4fv(mUMatrixLocation, 1, false, mProjectMatrix, 0);
         // 定义绘制的颜色
 //        glUniform4f(mUColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
         // 使用6个数字绘制2个三角形
